948291dee8896ff6484e206122f8625a90fbf8be
JungWoo Song <jsmac7014@gmail.com>
2026-01-29 21:31:54 +0900

    feat: ESP32 등록 엔드포인드

--- app/Http/Controllers/BLEAnchorController.php
+++ app/Http/Controllers/BLEAnchorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BleAnchor;
 use Illuminate\Http\Request;
 
 class BLEAnchorController extends Controller
@@ -17,10 +18,24 @@ class BLEAnchorController extends Controller
     /**
      * 새로운 ESP32 등록
      * @param Request $request
-     * @return void
+     * return string JSON-encoded BleAnchor object
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'anchor_uid' => 'required',
+            'room_id' => 'required|exists:rooms,id',
+            'label' => 'string|required',
+        ]);
 
+        $anchor = BleAnchor::create([
+            'anchor_uid' => $request->anchor_uid,
+            'room_id' => $request->room_id,
+            'label' => $request->label,
+            'tx_power_dbm' => $request->tx_power_dbm ?? 9,
+            'installed_at' => now(),
+        ]);
+
+        return json_encode($anchor);
     }
 }
